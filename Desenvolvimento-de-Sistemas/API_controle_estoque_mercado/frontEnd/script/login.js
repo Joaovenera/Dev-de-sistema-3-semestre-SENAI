1483550d302799a143feaf3f89aa4c9daabd25fa
SwissLemon <rafael_lemos1@estudante.sesisenai.org.br>
2024-10-29 21:47:16 -0300

    FIM dia 29_10

---
+++ Desenvolvimento-de-Sistemas/API_controle_estoque_mercado/frontEnd/script/login.js
@@ -0,0 +1,13 @@
+function validarLogin(email, senha) {
+    if (email) {
+        if (senha) {
+            
+        } else {
+            print("Erro senha")
+        }
+    } else {
+        print("Erro email")
+    }
+}
+
+validarLogin()
