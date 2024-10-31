2aa24ed57e16f4cd9567781e9b1a72d8a082bf08
SwissLemon <rafael_lemos1@estudante.sesisenai.org.br>
2024-10-31 19:49:57 -0300

    19:49

---
+++ Desenvolvimento-de-Sistemas/API_controle_estoque_mercado/frontEnd/script/cadastroProdutos.js
@@ -0,0 +1,23 @@
+import { apiCadastrarProduto } from './api';
+
+document.getElementById('cadastroForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const novoProduto = {
+        nome: document.getElementById('nome').value.trim(),
+        descricao: document.getElementById('descricao').value.trim(),
+        valor: document.getElementById('valor').value.trim(),
+        quantidade: document.getElementById('quantidade').value.trim()
+    };
+    console.log(novoProduto);
+
+    try {
+        await apiCadastrarProduto(novoProduto);
+        //document.getElementById('mensagemSucesso').style.display = 'block';
+        //document.getElementById('mensagemErro').style.display = 'none';
+        ///setTimeout(() => window.location.href = 'login.html', 2000);
+    } catch (error) {
+        //document.getElementById('mensagemErro').style.display = 'block';
+       // document.getElementById('mensagemSucesso').style.display = 'none';
+    }
+});
